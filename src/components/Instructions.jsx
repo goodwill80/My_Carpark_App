a6fbba3e55e4bab22d579d740324534af394764d
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-01-20 13:13:55 +0800

    instructions on homepage

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 function Instructions() {
   return (
-    <div className="flex flex-col text-sm mt-4 text-center w-[40%] text-red-500 font-semibold">
-      <h1>
-        Please enable your location settings in browser and device. Viewing is
-        best experienced using Chrome.
-      </h1>
+    <div className="flex flex-col text-sm mt-4 text-center w-[40%] text-orange-500 font-semibold">
+      <p>Please enable your location settings in browser and device.</p>
+      <p>Viewing is best experienced using Chrome.</p>
     </div>
   );
 }
