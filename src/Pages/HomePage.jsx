8c8d500b7f971c6580c5264a311ad5eabad8d59d
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-01-20 13:05:26 +0800

    instructions

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import { useContext } from 'react';
 import { CarparkContext } from '../Context/CarparkContext';
 import Form from '../components/Form';
+import Instructions from '../components/Instructions';
 
 function HomePage() {
   const { getUserData, setSignIn } = useContext(CarparkContext);
 
   return (
     <div className="flex flex-col justify-center items-center min-h-[100vh] h-auto pb-12">
+      <Instructions />
       <Form getUserData={getUserData} setSignIn={setSignIn} />
     </div>
   );
---
+++ src/components/Instructions.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Instructions() {
+  return (
+    <div className="flex flex-col text-sm mt-4 text-center w-[40%] text-red-500 font-semibold">
+      <h1>
+        Please enable your location settings in browser and device. Viewing is
+        best experienced using Chrome.
+      </h1>
+    </div>
+  );
+}
+
+export default Instructions;
