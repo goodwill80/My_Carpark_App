19d5e93a225685884f82a8e5541d3a9abbf5ac7e
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-01-20 13:17:08 +0800

    styling of instructions

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Instructions() {
   return (
-    <div className="flex flex-col text-sm mt-4 text-center w-[40%] text-orange-500 font-semibold">
-      <p>Please enable your location settings in browser and device.</p>
+    <div className="flex flex-col text-sm mt-4 text-center w-[80%] md:w-[60%] text-blue-500 font-semibold">
+      <p className="text-black font-semibold">
+        Please enable your location settings in browser and device.
+      </p>
       <p>Viewing is best experienced using Chrome.</p>
     </div>
   );
