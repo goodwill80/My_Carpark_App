7e7fe1e16735c332511d5ddad1d79335884d3840
Ho Kit Jonathan <hokitjonathan@Hos-MacBook-Air-2.local>
2023-01-21 11:38:09 +0800

    changed restriction msg

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -11,7 +11,7 @@ function HomePage() {
       <Instructions />
       <Form getUserData={getUserData} setSignIn={setSignIn} />
       <div className="flex flex-col justify-center items-center">
-        <p className="text-sm flex flex-col mt-4 text-center w-[80%] md:w-[37%] text-gray-400">
+        <p className="text-sm flex flex-col mt-4 text-center w-[80%] md:w-[32%] text-gray-400">
           A monthly restriction of API calls had been set for this website to
           avoid free limits from exceeding. We apologise for the experience if
           you encounter any restrictions using this app. This is a group project
